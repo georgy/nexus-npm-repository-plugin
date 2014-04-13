64980ea6ffd69352ae3b9fcc9e78b5eff52bae40
Georgy Bolyuba <georgy@bolyuba.com>
2014-04-13 17:47:52 +0200

    redeploy whole bundle including dependencies (need to unpack it, nexus does not do that)

--- redeploy.cmd
+++ redeploy.cmd
@@ -2,7 +2,15 @@ cmd /c "D:\tools\nexus\nexus-2.7.2-03\bin\nexus.bat stop"
 
 cmd /c "mvn package"
 
-xcopy /v /f /y target\nexus-npm-repository-plugin-0.0.1-SNAPSHOT.jar D:\tools\nexus\sonatype-work\nexus\plugin-repository\nexus-npm-repository-plugin-0.0.1-SNAPSHOT
+del /q D:\tools\nexus\sonatype-work\nexus\plugin-repository\nexus-npm-repository-plugin-0.0.1-SNAPSHOT
+
+xcopy /v /f /y target\nexus-npm-repository-plugin-0.0.1-SNAPSHOT-bundle.zip D:\tools\nexus\sonatype-work\nexus\plugin-repository\nexus-npm-repository-plugin-0.0.1-SNAPSHOT-bundle.zip
+
+pushd D:\tools\nexus\sonatype-work\nexus\plugin-repository\
+
+jar xf D:\tools\nexus\sonatype-work\nexus\plugin-repository\nexus-npm-repository-plugin-0.0.1-SNAPSHOT-bundle.zip
+
+popd
 
 del D:\tools\nexus\sonatype-work\nexus\logs\nexus.log
 
