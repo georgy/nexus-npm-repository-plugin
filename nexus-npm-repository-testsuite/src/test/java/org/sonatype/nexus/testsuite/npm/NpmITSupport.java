18423ba66aad8949c96a3929dca6c6c51fdf2c4f
Tamas Cservenak <tamas@cservenak.net>
2014-06-30 10:40:31 +0200

    NX-507: Removing incorrect hdrs and since versions

--- nexus-npm-repository-testsuite/src/test/java/org/sonatype/nexus/testsuite/npm/NpmITSupport.java
+++ nexus-npm-repository-testsuite/src/test/java/org/sonatype/nexus/testsuite/npm/NpmITSupport.java
@@ -1,15 +1,3 @@
-/*
- * Sonatype Nexus (TM) Open Source Version
- * Copyright (c) 2007-2014 Sonatype, Inc.
- * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
- *
- * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
- * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
- *
- * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
- * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
- * Eclipse Foundation. All other trademarks are the property of their respective owners.
- */
 package org.sonatype.nexus.testsuite.npm;
 
 import org.junit.AfterClass;
@@ -32,8 +20,6 @@ import static org.sonatype.sisu.goodies.common.Varargs.$;
 
 /**
  * Support for NPM integration tests.
- *
- * @since 2.9
  */
 @NexusStartAndStopStrategy(EACH_TEST)
 public abstract class NpmITSupport
